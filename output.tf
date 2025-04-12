b1aa7a0e46cb5392c87002518f05fce48f7e0f79
swamy527 <swamybandari527@gmail.com>
2025-04-12 07:27:29 +0530

    files updated5

--- output.tf
+++ output.tf
@@ -5,3 +5,15 @@ output "all_zones" {
 output "vpc_id" {
   value = aws_vpc.roboshop.id
 }
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnets" {
+  value = aws_subnet.database[*].id
+}
