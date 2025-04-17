e5587ab30ec8e44806b82d6cc7d3e3e835a84d1f
swamy527 <swamybandari527@gmail.com>
2025-04-17 18:38:39 +0530

    file updated

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_route" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.roboshop.id
 }
-resource "aws_route" "public" {
+resource "aws_route" "private" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.roboshop.id
